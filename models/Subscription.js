f98e0eef63f2cc60db48dfab27419fe884df20af
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-07-29 12:20:36 +0300

    added Subscription Schema

---
+++ models/Subscription.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const subscriptionSchema = new mongoose.Schema({
+    subName: {
+        type: String,
+        required: true
+    },
+    subCategory: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        requried: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"
+    }
+})
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema)
+
+module.exports = Subscription
