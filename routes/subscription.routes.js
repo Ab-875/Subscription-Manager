730965df56e15990a5df9222c08acb60d3a4d6a5
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-07-29 21:33:56 +0300

    fixed delete issue and route

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -71,7 +71,7 @@ router.put("/:id", async (req,res) => {
     }
 })
 
-router.delete("/id", async (req,res) => {
+router.delete("/:id", async (req,res) => {
     try{
         await  Subscription.findByIdAndDelete(req.params.id)
         res.redirect("/subscriptions")
